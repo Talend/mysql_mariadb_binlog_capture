6275c791701a89b4987e974718812a2e25f7a9eb
Etienne Chauchot <echauchot@apache.org>
2022-07-01 10:33:14 +0200

    Change Talend Copyright date from 2021-2022 to 2022

--- ddMySqlBinLogCapture/synchronization.h
+++ ddMySqlBinLogCapture/synchronization.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021-2022 Talend Inc. - www.talend.com
+/* Copyright (C) 2022 Talend Inc. - www.talend.com
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
